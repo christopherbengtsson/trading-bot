c5317b1e69ba494bf5e8a879a0fd347ba9252d29
Christopher Bengtsson <christopher.bengtsson@trustly.com>
2021-10-24 14:31:25 +0200

    strategy -> look at entry candle if above ema

--- strategy.py
+++ strategy.py
@@ -36,12 +36,13 @@ def set_up_dataframe(data):
 
 
 def check_strategy_signal(df):
-    last_closed_index = len(df) - 2
+    entry_candle_index = len(df) - 1
+    last_closed_index = entry_candle_index - 1
     previous_closed_index = last_closed_index - 1
 
-    last_opened_price = df['open'][last_closed_index]
-    last_closed_price = df['close'][last_closed_index]
-    last_closed_ema = df['ema200'][last_closed_index]
+    entry_candle_open = df['open'][entry_candle_index]
+    entry_candle_close = df['close'][entry_candle_index]
+    entry_candle_ema = df['ema200'][entry_candle_index]
 
     last_closed_macd_line = df['macd_line'][last_closed_index]
     last_closed_macd_signal_line = df['macd_signal_line'][last_closed_index]
@@ -50,16 +51,11 @@ def check_strategy_signal(df):
     previous_closed_macd_signal_line = df['macd_signal_line'][previous_closed_index]
 
     # Check candle was opened and closed above EMA 200
-    if last_closed_price > last_closed_ema and last_opened_price > last_closed_ema:
+    if entry_candle_open > entry_candle_ema and entry_candle_close > entry_candle_ema:
         # Check for MACD crossover above 0 line
         if previous_closed_macd_line <= previous_closed_macd_signal_line and last_closed_macd_line > last_closed_macd_signal_line and last_closed_macd_line < 0:
             return SIDE_BUY
-    # Check candle was opened and closed below EMA 200
-    elif last_closed_price < last_closed_ema and last_opened_price < last_closed_ema:
-        # return SIDE_SELL
-        return 'niet'
 
 # Buy when the MACD line crosses above the signal line, below the zero line if entry candle is above 200 ema
 # Stoploss at the lowest point from last 10 candles
 # Set profit target 1.5 times stoploss
-
